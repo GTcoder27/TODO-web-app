0cf5fb10202053e6b04974959dc5db0bc0c81588
GTcoder27 <girishnandanwar2708@gmail.com>
2024-09-18 08:49:27 +0530

    added TODO app

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// require('.env').config();
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(() => console.log("Connected to MongoDB Atlas"))
+// .catch(err => console.error("Error connecting to MongoDB Atlas", err));
 const express = require('express');
 const app = express();
 const path = require('path');
--- models/user.js
+++ models/user.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch(err => console.error("Error connecting to MongoDB Atlas", err));
 
 const userSchema = mongoose.Schema({
     username:String,
